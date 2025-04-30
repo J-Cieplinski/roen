341e5498d70680b9f92b8ada94838cce46cd83cb
Jakub Cieplinski <jakub.cieplinski@outlook.com>
2025-04-30 10:51:50 +0200

    Added LUA specific logger

--- include/log/Logger.hpp
+++ include/log/Logger.hpp
@@ -32,13 +32,16 @@ public:
 
     static void setSdkLogLevel(spdlog::level::level_enum level);
     static void setAppLogLevel(spdlog::level::level_enum level);
+    static void setLuaLogLevel(spdlog::level::level_enum level);
 
     static std::shared_ptr<spdlog::logger>& getSdkLogger();
     static std::shared_ptr<spdlog::logger>& getAppLogger();
+    static std::shared_ptr<spdlog::logger>& getLuaLogger();
 
 private:
     inline static std::shared_ptr<spdlog::logger> sdkLogger_;
     inline static std::shared_ptr<spdlog::logger> appLogger_;
+    inline static std::shared_ptr<spdlog::logger> luaLogger_;
 };
 
 }  // namespace roen::log
@@ -65,6 +68,15 @@ private:
 #    define SET_APP_LOG_LEVEL(...) roen::log::Logger::setAppLogLevel(__VA_ARGS__)
 #    define GET_APP_LOG_LEVEL() roen::log::Logger::getAppLogger()->level()
 
+// Lua log macros
+#    define LUA_ERROR(...) roen::log::Logger::getLuaLogger()->error(__VA_ARGS__)
+#    define LUA_INFO(...) roen::log::Logger::getLuaLogger()->info(__VA_ARGS__)
+#    define LUA_WARN(...) roen::log::Logger::getLuaLogger()->warn(__VA_ARGS__)
+#    define LUA_TRACE(...) roen::log::Logger::getLuaLogger()->trace(__VA_ARGS__)
+#    define LUA_CRITICAL(...) roen::log::Logger::getLuaLogger()->critical(__VA_ARGS__)
+#    define SET_LUA_LOG_LEVEL(...) roen::log::Logger::setLuaLogger(__VA_ARGS__)
+#    define GET_LUA_LOG_LEVEL() roen::log::Logger::getLuaLogger()->level()
+
 #else
 #    define SDK_ERROR(...)
 #    define SDK_INFO(...)
@@ -82,6 +94,15 @@ private:
 #    define APP_CRITICAL(...)
 #    define SET_APP_LOG_LEVEL(...)
 #    define GET_APP_LOG_LEVEL() LOG_LEVEL_INFO
+
+// Lua log macros
+#    define LUA_ERROR(...)
+#    define LUA_INFO(...)
+#    define LUA_WARN(...)
+#    define LUA_TRACE(...)
+#    define LUA_CRITICAL(...)
+#    define SET_LUA_LOG_LEVEL(...)
+#    define GET_LUA_LOG_LEVEL() LOG_LEVEL_INFO
 #endif  // IS_DEBUG
 
 #endif  // ROEN_LOG_LOGGER_HPP
--- src/log/Logger.cpp
+++ src/log/Logger.cpp
@@ -8,11 +8,15 @@ namespace roen::log
 void Logger::Init()
 {
     spdlog::set_pattern("%^[%T] %n: %v%$");
+
     sdkLogger_ = spdlog::stdout_color_mt("SDK");
     sdkLogger_->set_level(spdlog::level::trace);
 
     appLogger_ = spdlog::stderr_color_mt("APP");
     appLogger_->set_level(spdlog::level::trace);
+
+    luaLogger_ = spdlog::stderr_color_mt("LUA");
+    luaLogger_->set_level(spdlog::level::trace);
 }
 
 void Logger::setSdkLogLevel(spdlog::level::level_enum level)
@@ -33,6 +37,15 @@ void Logger::setAppLogLevel(spdlog::level::level_enum level)
     appLogger_->set_level(level);
 }
 
+void Logger::setLuaLogLevel(spdlog::level::level_enum level)
+{
+    if (!appLogger_)
+    {
+        throw std::runtime_error("LUA LOGGER was not initialized");
+    }
+    appLogger_->set_level(level);
+}
+
 std::shared_ptr<spdlog::logger>& Logger::getSdkLogger()
 {
     if (!sdkLogger_)
@@ -49,6 +62,15 @@ std::shared_ptr<spdlog::logger>& Logger::getAppLogger()
         throw std::runtime_error("APP LOGGER was not initialized");
     }
     return appLogger_;
+};
+
+std::shared_ptr<spdlog::logger>& Logger::getLuaLogger()
+{
+    if (!luaLogger_)
+    {
+        throw std::runtime_error("LUA LOGGER was not initialized");
+    }
+    return luaLogger_;
 };
 
 }  // namespace roen::log
